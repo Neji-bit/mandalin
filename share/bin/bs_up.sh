a6da853b8f9ca8e3b38b7c1c432dc111ba267b1b
Neji <neji.bit@gmail.com>
2023-02-08 21:37:01 +0900

    up.

--- share/bin/bs_up.sh
+++ share/bin/bs_up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 which browser-sync || npm install -g browser-sync
 cd $_RAILS_DIR
-browser-sync start --config /share/bin/bs-config.js &
+browser-sync start --config /share/bin/bs-config.js >> $_RAILS_DIR/log/browser-sync.log 2>> $_RAILS_DIR/log/browser-sync.error.log &
 exit
